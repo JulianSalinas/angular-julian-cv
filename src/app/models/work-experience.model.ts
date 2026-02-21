a499e835237618a10011bc023b14d9f537de6474
Julian Salinas <js-julian.salinas@outlook.com>
2026-02-21 14:31:30 -0600

    Adding clientExperience

--- src/app/models/work-experience.model.ts
+++ src/app/models/work-experience.model.ts
@@ -4,6 +4,13 @@ export interface WorkRole {
   endDate: string;
 }
 
+export interface ClientExperience {
+  client: string;
+  startDate: string;
+  endDate: string;
+  highlights: string[];
+}
+
 export interface WorkExperience {
   id: number;
   position: string;
@@ -11,6 +18,7 @@ export interface WorkExperience {
   startDate: string;
   endDate: string;
   client?: string;
+  clientExperience?: ClientExperience[];
   roles?: WorkRole[];
   highlights: string[];
 }
